54e9a18121fc9b92f22842d6e7754265105068aa
jyotishiota <joyitsh.iotait@gmail.com>
2023-07-13 16:42:27 +0600

    change alarm

--- app/src/main/java/com/example/exactalarmdemo/MainActivity.kt
+++ app/src/main/java/com/example/exactalarmdemo/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.exactalarmdemo
 
 import android.app.*
+import android.content.ContentValues
 import android.content.Intent
+import android.icu.text.SimpleDateFormat
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.PowerManager
 import android.provider.Settings
 import android.provider.Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM
+import android.text.format.DateFormat
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,7 +39,7 @@ class MainActivity : AppCompatActivity() {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 val calendar = Calendar.getInstance()
                 //10 is for how many seconds from now you want to schedule also you can create a custom instance of Callender to set on exact time
-                calendar.add(Calendar.MINUTE, 10)
+                calendar.add(Calendar.MINUTE, 20)
                 //function for Creating [Notification Channel][1]
                 createNotificationChannel()
                 //function for scheduling the notification
@@ -59,6 +64,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun scheduleNotification(calendar: Calendar) {
+        Log.d(ContentValues.TAG, "scheduleAlarm:current time ${System.currentTimeMillis()}")
+        Log.d(ContentValues.TAG, "scheduleAlarm:current time ${calendar.timeInMillis }")
+        val timeinmilli = calendar.timeInMillis //will give tou time in ms
+
+
+        val formatter = SimpleDateFormat("dd/MM/yyyy") //format of the date, choose what you want
+
+
+        val resultdate = Date(timeinmilli) //create an object date associated to the time in ms
+
+        Log.e("lol", resultdate.toString())
+
+
+        val newTime: Long
+
         val intent = Intent(applicationContext, MyNotificationPublisher::class.java)
         intent.putExtra("titleExtra", "Dynamic Title")
         intent.putExtra("textExtra", "Dynamic Text Body")
@@ -70,7 +90,7 @@ class MainActivity : AppCompatActivity() {
         )
         val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
         alarmManager.setExactAndAllowWhileIdle(
-            AlarmManager.ELAPSED_REALTIME_WAKEUP,
+            AlarmManager.RTC_WAKEUP,
             calendar.timeInMillis,
             pendingIntent
         )
